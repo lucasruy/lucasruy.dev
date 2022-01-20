992a491ccd0d380980c6b2d5278399f75e5efab4
lucasruy <lucasruy@sas.com.br>
2022-01-20 19:19:00 -0300

    feat: change project status name

--- src/components/projects/project-status.js
+++ src/components/projects/project-status.js
@@ -5,7 +5,7 @@ const ProjectStatus = ({ status }) => (
   <li className="inline-block px-3 py-1 mr-1 mt-1 font-medium text-xs rounded-lg border border-back">
     <span
       className={`inline-block w-2 h-2 rounded-full mr-2 ${
-        status === "live" ? "bg-green-500" : "bg-orange-500"
+        status === "publicado" ? "bg-green-500" : "bg-green-500-400"
       }`}
     ></span>
     Status: {status}
