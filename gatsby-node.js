12cf671e66ad7343368a75133369402bbe928c14
lucasruy <lucasruy@sas.com.br>
2022-01-20 20:23:52 -0300

    feat: remove unnecessary packages imports

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,3 @@
-const path = require("path")
-const fs = require("fs")
-const pkg = require("./package.json")
-
 exports.createPages = require("./src/gatsby/node/createPages")
 exports.onPreInit = require("./src/gatsby/node/onPreInit")
 exports.onPreBootstrap = require("./src/gatsby/node/onPreBootstrap")
