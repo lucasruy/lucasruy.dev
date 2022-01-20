26926f8b4b76d981c2046d6d08d1040f743a0e2a
lucasruy <lucasruy@sas.com.br>
2022-01-20 20:19:18 -0300

    feat: add rename build folder support

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,9 @@
-exports.onPreBootstrap = require("./src/gatsby/node/onPreBootstrap")
-exports.sourceNodes = require("./src/gatsby/node/sourceNodes")
+const path = require("path")
+const fs = require("fs")
+const pkg = require("./package.json")
+
 exports.createPages = require("./src/gatsby/node/createPages")
+exports.onPreInit = require("./src/gatsby/node/onPreInit")
+exports.onPreBootstrap = require("./src/gatsby/node/onPreBootstrap")
+exports.onPostBuild = require("./src/gatsby/node/onPostBuild")
+exports.sourceNodes = require("./src/gatsby/node/sourceNodes")
